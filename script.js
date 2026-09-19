4752033ab840a39ef6d1c5f041d31f6677aeb885
AdineniVigneshRaghavendra <adinenivignesh09@gmail.com>
2026-09-19 21:38:26 +0530

    cursor settings

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ const message = document.getElementById("message");
 const walls = document.querySelectorAll(".wall");
 const start = document.querySelector(".start");
 const exit = document.querySelector(".exit");
+const fakeCursor = document.getElementById("fakeCursor");
+const jumpscare = document.getElementById("jumpscare");
+
+let lastMouseX = 0;
+let lastMouseY = 0;
+
 
 let gameStarted = false;
 let resetting = false;
 
-start.addEventListener("mouseenter", () => {
+start.addEventListener("click", () => {
     if(resetting) return;
 
     gameStarted = true;
@@ -15,7 +21,11 @@ start.addEventListener("mouseenter", () => {
     message.textContent = "Find the exit";
 });
 
-maze.addEventListener("mouseenter", (event) => {
+maze.addEventListener("mousemove", (event) => {
+
+    lastMouseX = event.clientX;
+    lastMouseY = event.clientY;
+
     if(!gameStarted || resetting) {
         return;
     }
@@ -33,18 +43,63 @@ maze.addEventListener("mouseenter", (event) => {
             wallHit();
             return;
         }
-    }
+     }
+    });
 
-    const exitRect = exit.getBoundingClientRect();
+   exit.addEventListener("click", () => {
+    if(!gameStarted || resetting) return;
 
-    if(
-        mouseX >= exitRect.left &&
-        mouseX <= exitRect.right &&
-        mouseY >= exitRect.top &&
-        mouseY <= exitRect.bottom
-    ) {
-        reachedExit();
-    }
-});
+    reachedExit();
+   });
 
-function 
+function wallHit() {
+    gameStarted = false;
+    resetting = true;
+
+    message.textContent = "YOU TOUCHED THE WALL.";
+
+    setTimeout(() => {
+        message.textContent = "Try again.";
+
+        resetting=false;
+    }, 1000);
+}
+function reachedExit() {
+    gameStarted = false;
+    message.textContent = "...";
+    setTimeout(() => {
+        message.textContent = "WAIT.";
+    }, 1500);
+
+    setTimeout(() => {
+        message.textContent = "Something is wrong.";
+
+        document.body.style.cursor = "none";
+
+        fakeCursor.style.display = "block";
+        fakeCursor.style.left = lastMouseX + "px";
+        fakeCursor.style.top =  lastMouseY + "px";
+        setTimeout(() => {
+            fakeCursor.style.transition = "left 2s ease";
+            fakeCursor.style.left = (lastMouseX + 80) + "px";
+        }, 1000);
+        setTimeout(() => {
+
+            fakeCursor.style.transition = "top 2s ease";
+
+            fakeCursor.style.top = (lastMouseY + 60) + "px";
+        }, 4500);
+        setTimeout(() => {
+            jumpscare.style.display = "block";
+
+            setTimeout(() => {
+                jumpscare.style.display = "none";
+            }, 100);
+        }, 7000);
+
+    }, 3000);
+
+    setTimeout(() => {
+        message.textContent = "DON'T MOVE YOUR MOUSE.";
+    }, 5000);
+}
